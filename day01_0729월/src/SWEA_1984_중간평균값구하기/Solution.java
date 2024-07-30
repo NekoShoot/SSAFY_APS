99fca2282c69e6fa0a56a57bb268cc3d6b4b330c
조홍균 <whghdrbs1002@gmail.com>
2024-07-31 08:18:32 +0900

    add 1984 2068 problems

--- day01_0729월/src/SWEA_1984_중간평균값구하기/Solution.java
+++ day01_0729월/src/SWEA_1984_중간평균값구하기/Solution.java
@@ -1,5 +1,65 @@
 package SWEA_1984_중간평균값구하기;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		try {
+			Scanner sc = new Scanner(System.in);
+			
+			int T = sc.nextInt();
+			for(int test_case = 1; test_case <= T; test_case++) {
+				
+				
+				int[] arr = new int[10];
+				
+				int sum = 0;
+				int max = 0;
+				int min = 10000 * 10 + 1; // 모든 최대값의 합 보다 1 큰 값
+				
+				for(int i = 0; i < 10; i++) {
+					arr[i] = sc.nextInt();
+					
+					if(arr[i] > max) { // max 저장
+						max = arr[i];
+					} 
+					
+					if(arr[i] < min) {
+						min = arr[i];
+					}			
+									
+				}
+				
+				if(max == min) {
+					System.out.printf("#%d %d\n", test_case, 0);
+					continue;
+				}
+					
+				for(int i = 0; i < 10; i++) {
+					if(arr[i] == min || arr[i] == max) {
+						continue;
+						
+					} else {
+						sum += arr[i];						
+						
+					}
+					
+				}
+				
+				int result;
+				double divided = (double) sum / 8;
+				double underPoint = (divided * 10) % 10;
+				if(underPoint >= 5) {
+					result = (sum / 8) + 1;
+				} else {
+					result = (sum / 8);
+				}
+					
+				System.out.printf("#%d %d\n", test_case, result);
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}		
+	}
 }
--- day01_0729월/src/SWEA_2068_최대수구하기/Solution.java
+++ day01_0729월/src/SWEA_2068_최대수구하기/Solution.java
@@ -1,5 +1,23 @@
 package SWEA_2068_최대수구하기;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+ 
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int result = 0;
+             
+            for(int i = 0; i < 10; i++) {
+                int tmp = sc.nextInt();
+                if(tmp > result) result = tmp;             
+            }
+             
+            System.out.printf("#%d %s\n", test_case, result);
+        }
+	}
 }
