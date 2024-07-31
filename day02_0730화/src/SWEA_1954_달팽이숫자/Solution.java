4a736d669e0d48ad0e37387d0fcca6e97fd2561c
조홍균 <whghdrbs1002@gmail.com>
2024-07-31 11:21:34 +0900

    commit

--- day02_0730화/src/SWEA_1954_달팽이숫자/Solution.java
+++ day02_0730화/src/SWEA_1954_달팽이숫자/Solution.java
@@ -1,5 +1,12 @@
 package SWEA_1954_달팽이숫자;
 
+import java.util.Arrays;
+
 public class Solution {
-	
+	public static void main(String[] args) {	
+		int r = 2, c = 2;
+		System.out.println(r);
+		System.out.println(c);
+		
+	}
 }
