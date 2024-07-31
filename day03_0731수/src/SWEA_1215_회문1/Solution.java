c7c40d0f3d053c8c533e783b0ee93cfcc2572291
NekoShoot <honggyunjo1109@gmail.com>
2024-07-31 23:13:33 +0900

    1215 reverse_word completed

--- day03_0731수/src/SWEA_1215_회문1/Solution.java
+++ day03_0731수/src/SWEA_1215_회문1/Solution.java
@@ -2,9 +2,7 @@ package SWEA_1215_회문1;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
@@ -14,23 +12,61 @@ public class Solution {
 		for(int test_case = 1; test_case <= 10; test_case++) {
 			int N = sc.nextInt(); // 단어를 이루는 글자 개수
 			
-			char[][] worldBoard = new char[8][8];
+			// 글자들을 저장할 배열 생성
+			char[][] wordBoard = new char[8][8];
 			
+			// 2중 반복으로 2차원 배열에 각 값 저장
 			for(int i = 0; i < 8; i++) {
 				String line = sc.next();	
 				for(int j = 0; j < 8; j++) {
-					worldBoard[i][j] = line.charAt(j);
+					wordBoard[i][j] = line.charAt(j);					
 					
-					
-				}				 
-				
-				
+				}	
 			}
-						
 			
-			System.out.println(Arrays.deepToString(worldBoard));
-		}
-		
+			// 개수 저장할 변수 생성
+			int cnt = 0;
+			
+			// 3중 반복으로 각각의 배열에서 N길이 만큼 탐색(가로,세로)
+			for(int i = 0; i < 8; i++) {				
+				for(int j = 0; j < 8; j++) {
+					String rowStr = "";
+					String colStr = "";
+					
+					// N만큼의 길이 가로 세로 더해서 단어 만들기
+					for(int m = 0; m < N; m++) {
+						// 경계 체크 로직
+						if(j+m < 8)	rowStr += wordBoard[i][j+m];
+						if(i+m < 8) colStr += wordBoard[i+m][j];
+					}									
+					
+					// 회문이면 cnt++					
+					if(isReverseSame(rowStr, N)) cnt++;
+					if(isReverseSame(colStr, N)) cnt++;
+
+
+				}
+			}
+			
+			System.out.printf("#%d %d\n", test_case, cnt);
+			
+		}		
 
 	}
+	
+	// 회문인지 판별해주는 메소드
+	static boolean isReverseSame(String word, int N) {
+		if(word.length() != N) return false;
+		String reversed = "";
+						
+		for(int i = word.length()-1; i >= 0; i--) {
+			// 거꾸로 더해서 만들기
+			reversed += word.charAt(i);		
+		}
+		
+		if(word.equals(reversed)) return true;
+			else return false;
+		
+	}
+	
 }
