f8c3260b0879952afb435fe42b58b051aef68cd4
NekoShoot <honggyunjo1109@gmail.com>
2024-08-27 17:05:26 +0900

    ongoing cook

--- day14_0827화/src/SWEA_4012_요리사/Solution.java
+++ day14_0827화/src/SWEA_4012_요리사/Solution.java
@@ -11,7 +11,21 @@ public class Solution {
 		int T = sc.nextInt();
 		//TODO 1 => T로 바꾸기
 		for(int test_case = 1; test_case <= 1; test_case++) {
-			int N = sc.nextInt();		
+			int N = sc.nextInt(); // 재료 개수
+			int[][] synergy = new int[N][N];
+			
+			// 시너지
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					synergy[i][j] = sc.nextInt();
+				}
+			}
+						
+			// 0 1 ... (N/2)-1까지 선택하는 경우의 수부터 시작
+			// N/2 ... N-1까지 선택하는 경우의 수로 끝
+			int[] memo = new int[N];			
+			
+		
 			
 		}
 	}
